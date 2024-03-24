13aabbcc5857f94b263fd989f89a6b6d1252e2c0
yushiang <tsengyushiang@gmail.com>
2024-03-24 18:35:52 +0800

    feature: setup sqlalchemy

--- server/app.py
+++ server/app.py
@@ -1,5 +1,12 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
