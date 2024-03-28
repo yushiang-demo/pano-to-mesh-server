56a9fe2d315ec8b8c6c2d62a2973260be3c6ab6d
yushiang <tsengyushiang@gmail.com>
2024-03-28 23:58:47 +0800

    refactor: folder structure and add flask-Migrate

--- server/app.py
+++ server/app.py
@@ -1,16 +1,17 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from routes import api_bp
+from extensions import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.app_context().push()
+
+app.register_blueprint(api_bp)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
-
-db = SQLAlchemy(app)
-
-@app.route("/")
-def hello():
-    return "Hello, World!"
+db.init_app(app)
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
---
+++ server/models/user.py
@@ -0,0 +1,9 @@
+from extensions import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=True, nullable=False)
+    time = db.Column(db.Date, default=datetime.utcnow)
